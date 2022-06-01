d9dd6abbb671386ea330da1f82b7ec39c629d4de
Rishab Kedia <risab86515@gmail.com>
2022-06-01 11:38:04 +0530

    streamlit

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ st.sidebar.title("What's Food Vision ?")
 st.sidebar.write("""
 FoodVision is an end-to-end **CNN Image Classification Model** which identifies the food in your image. 
 
-It can identify over 100 different food classes
+It can identify over 101 different food classes
 
-It is based upom a pre-trained Image Classification Model that comes with Keras and then retrained on the infamous **Food101 Dataset**.
+It is based upom a pre-trained Image Classification Model that comes with Keras and then retrained on the **Food101 Dataset**.
 
 **Accuracy :** **`85%`**
 
@@ -44,7 +44,12 @@ It is based upom a pre-trained Image Classification Model that comes with Keras 
 **Dataset :** **`Food101`**
 """)
 
+st.sidebar.markdown(body="""
 
+<th style="border:None"><a href="https://www.linkedin.com/in/rishab-kedia-iiitg/" target="blank"><img align="center" src="https://bit.ly/3wCl82U" alt="" height="40" width="40" /></a></th>
+<th style="border:None"><a href="https://github.com/Rishab86515" target="blank"><img align="center" src="https://bit.ly/3c2onZS" alt="rishab86515" height="40" width="40" /></a></th>
+
+""", unsafe_allow_html=True)
 #### Main Body ####
 
 st.title("Food Vision 🍔📷")
@@ -56,16 +61,6 @@ file = st.file_uploader(label="Upload an image of food.",
 
 model = tf.keras.models.load_model("EfficientNetB1.hdf5")
 
-
-st.sidebar.markdown("Created by **Gaurav Reddy**")
-st.sidebar.markdown(body="""
-
-<th style="border:None"><a href="https://twitter.com/gaurxvreddy" target="blank"><img align="center" src="https://bit.ly/3wK17I6" alt="gaurxvreddy" height="40" width="40" /></a></th>
-<th style="border:None"><a href="https://linkedin.com/in/gauravreddy08" target="blank"><img align="center" src="https://bit.ly/3wCl82U" alt="gauravreddy08" height="40" width="40" /></a></th>
-<th style="border:None"><a href="https://github.com/gauravreddy08" target="blank"><img align="center" src="https://bit.ly/3c2onZS" alt="16034820" height="40" width="40" /></a></th>
-<th style="border:None"><a href="https://instagram.com/gaurxv_reddy" target="blank"><img align="center" src="https://bit.ly/3oZABHZ" alt="gaurxv_reddy" height="40" width="40" /></a></th>
-
-""", unsafe_allow_html=True)
 
 if not file:
     st.warning("Please upload an image")
